95b9463a9e6c49b6577ea6efe4242c5f459b9e08
imshashankkumar057 <imshashankkumar057@gmail.com>
2024-12-11 18:47:29 +0530

    Add Vercel configuration for React Router

--- grocery-app/src/App.js
+++ grocery-app/src/App.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
-import SearchPage from './pages/SearchPage'; // This will be rendered first
-import CheckoutPage from './pages/CheckoutPage';
-import Logout from './components/Logout';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SearchPage from "./pages/SearchPage";
+import CheckoutPage from "./pages/CheckoutPage";
 
 function App() {
   return (
-    <Router basename="/grocery-fb">
+    <Router>
       <Routes>
-        {/* Change the default route ("/") to render SearchPage */}
         <Route path="/" element={<SearchPage />} />
-        {/* <Route path="/grocery-fb" element={<SearchPage />} /> */}
-        <Route path="/register" element={<Register />} />
         <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/logout" element={<Logout />} />
-        {/* <Route path="/login" element={<Login />} /> */}
+        {/* Fallback for undefined routes */}
+        <Route path="*" element={<SearchPage />} />
       </Routes>
     </Router>
   );
