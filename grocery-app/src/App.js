890c7d8a0f59db8dde7f62a68453f738442a7b7e
imshashankkumar057 <imshashankkumar057@gmail.com>
2024-12-11 19:00:26 +0530

    vercel config

--- grocery-app/src/App.js
+++ grocery-app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import SearchPage from "./pages/SearchPage";
 import CheckoutPage from "./pages/CheckoutPage";
 
